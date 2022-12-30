1ff6e4c902a8645cc6b3f868abdcb61c6b0c2cc0
Your Name <you@example.com>
2022-12-30 21:12:51 +0500

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import Logo from "@/components/icons/Logo.vue";
+</script>
+
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
